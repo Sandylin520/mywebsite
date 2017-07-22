f26b816e2dd639b49b5787f380c60bd40d4ff2e2
Sandylin810521 <b810521@gmail.com>
2017-07-22 22:18:26 +0800

    JS 0722
    Prompy alert practice 0722

---
+++ JS.js
@@ -0,0 +1,13 @@
+var firstName = prompt("What is your name?");
+var lastName = prompt("What is your lastName?");
+alert(" Your name "+ firstName + ""+ lastName + " is such a beautiful name")
+var Age = prompt("What is your age?");
+alert(" Wow! You are only " + Age + " years old, your are pretty young!! ")
+
+var fullName = firstName + " " + lastName
+/*console.log("Hello! " + firstName + " " + lastName);   */
+console.log("Hello! " + fullName);
+console.log("Your age is "+ Age);
+
+var year = prompt("How many year have you lived?");
+alert( year + " years is about " + year*365 + " days! ");
